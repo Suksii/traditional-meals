0bbb017e998d56dbe3e08c1d9dc266f5284ac4f3
unknown <ramovic225@gmail.com>
2024-12-24 23:57:52 +0100

    Meals component styled with framer motion

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Meals = ({ title = "", descriptions = [], image, list = [] }) => {
   return (
     <div className="md:min-h-screen flex flex-col justify-center gap-8 p-6 lg:w-3/4 mx-auto">
       <div className="flex flex-col md:flex-row items-center gap-8">
-        <div className="w-full md:w-1/2 relative">
+        <motion.div
+          className="w-full md:w-1/2 relative"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
           <img
             src={image.src}
             alt={image.alt}
@@ -14,12 +20,15 @@ const Meals = ({ title = "", descriptions = [], image, list = [] }) => {
           <div className="absolute bottom-0 py-2 bg-gray-600 bg-opacity-70 text-gray-50 font-semibold rounded-b-lg w-full text-center">
             {image.desc}
           </div>
-        </div>
+        </motion.div>
 
-        <div
+        <motion.div
           className={`w-full md:w-1/2 space-y-4 ${
             descriptions.length > 0 && "md:space-y-10"
           }`}
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 0.5 }}
         >
           <h1 className="text-2xl font-bold text-gray-800 mb-4">{title}</h1>
           {descriptions &&
@@ -34,13 +43,16 @@ const Meals = ({ title = "", descriptions = [], image, list = [] }) => {
                 {description.description}
               </p>
             ))}
-        </div>
+        </motion.div>
       </div>
 
-      <div
+      <motion.div
         className={`space-y-4 ${
           descriptions.length > 0 ? "md:space-y-10" : "pb-10"
         } w-full mx-auto`}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 0.7 }}
       >
         {descriptions &&
           descriptions.slice(3).map((description) => (
@@ -54,7 +66,7 @@ const Meals = ({ title = "", descriptions = [], image, list = [] }) => {
               {description.description}
             </p>
           ))}
-      </div>
+      </motion.div>
     </div>
   );
 };
