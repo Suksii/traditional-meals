489594c7e1a8291902bbde858424e7a3ae4676a5
unknown <ramovic225@gmail.com>
2024-12-25 00:16:19 +0100

    Intro component styled with framer-motion

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TraditionalMeal from "../assets/traditional-meal.png";
+import { motion } from "framer-motion";
 import BgImage from "../assets/bg-image.jpg";
 const Intro = () => {
   return (
@@ -7,12 +8,27 @@ const Intro = () => {
       className="min-h-screen bg-blue-100 w-full flex flex-col justify-center items-center bg-no-repeat bg-cover relative"
       style={{ backgroundImage: `url(${BgImage})` }}
     >
-      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-      <h1 className="text-2xl md:text-3xl font-semibold text-center py-10 text-white z-10">
+      <motion.div
+        className="absolute inset-0 bg-black bg-opacity-50"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 0.5 }}
+        transition={{ duration: 1 }}
+      ></motion.div>
+      <motion.h1
+        className="text-2xl md:text-3xl font-semibold text-center py-10 text-white z-10"
+        initial={{ opacity: 0, y: -50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, delay: 0.5 }}
+      >
         Tradicijom do digitalizacije
-      </h1>
+      </motion.h1>
       <div className="w-full md:w-2/3 px-4 text-center space-y-12 text-white z-10">
-        <p className="text-lg md:text-xl">
+        <motion.p
+          className="text-lg md:text-xl"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 0.5 }}
+        >
           Cilj ovog projekta je isključivo promocija bošnjačke kulture I
           tradicije kroz nacionalnu kuhinju. Svjedoci smo da ljudima prezentuju
           I promovišu razni trendovi koje dovode do gubitka fundamentalnih
@@ -25,14 +41,19 @@ const Intro = () => {
           očuvanju bošnjačkog nacionalnog identiteta. Digtalizacijom mladim
           ljudima olakšavamo pristup čime se ostvaruje cilj podizanja svijesti I
           interesovanja kod ove populacije za ovim segmentom tradicije.
-        </p>
-        <p className="text-lg md:text-xl">
+        </motion.p>
+        <motion.p
+          className="text-lg md:text-xl"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 0.7 }}
+        >
           Lice zaduženo u okviru ove aktivnosti je Fetahović Ernad koji je
           obavljao razgovore i susrete sa pripadnicima starije populacije koji
           posjeduju znanja i vještine iz ove oblasti. Nalon toga izvršio je
           preciznu selekciju sadržaja i formirao recepate sa istaknutim
           specifičnostima za pojedina nacionalna jela.
-        </p>
+        </motion.p>
       </div>
     </div>
   );
